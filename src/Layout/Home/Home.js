4de0c4c6a78168769bef33f7f4af19d86122210b
Jer Ber <jeremybuenaventura@Dollys-MacBook-Air.local>
2022-09-08 16:45:28 -0700

    initial commit

---
+++ src/Layout/Home/Home.js
@@ -0,0 +1,18 @@
+
+import React from "react";
+import {Link} from "react-router-dom";
+import ListDecks from "../Decks/ListDecks";
+
+function Home() {
+    return (
+        <div>
+            <Link to="/decks/new" className="btn btn-secondary mx-3 mb-3">+Create Deck</Link>
+            <br/>
+            <div className="container">
+            <ListDecks/>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
